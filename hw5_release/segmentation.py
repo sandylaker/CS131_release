4a1316615f0b799551805c7dfbff1ae20afe487a
sandylaker <lfc199471@gmail.com>
2019-07-31 19:24:35 +0200

    HW5 done

--- hw5_release/segmentation.py
+++ hw5_release/segmentation.py
@@ -12,7 +12,8 @@ import random
 from scipy.spatial.distance import squareform, pdist
 from skimage.util import img_as_float
 
-### Clustering Methods
+
+# Clustering Methods
 def kmeans(features, k, num_iters=100):
     """ Use kmeans algorithm to group features into k clusters.
 
@@ -44,11 +45,30 @@ def kmeans(features, k, num_iters=100):
     assignments = np.zeros(N)
 
     for n in range(num_iters):
-        ### YOUR CODE HERE
-        pass
-        ### END YOUR CODE
+        # YOUR CODE HERE
+        # compute the distances between points and cluster centers
+        # Can use the x1.T * x2 / (||x1|| * ||x2||) as a measurement for the closeness here
+        # euclid_dist = np.sum(features.reshape(N, 1, -1) * centers.reshape(1, D, -1), axis=-1)
+        # norms = np.linalg.norm(features, axis=-1, keepdims=True) * np.linalg.norm(
+        #     centers, axis=-1, keepdims=True).T
+        # dist = euclid_dist / norms
+        # # assignments_new = np.argmax(dist, axis=-1)
+        # assign points to clusters
+
+        dist = np.sum((features.reshape(N, 1, D) - centers.reshape(1, -1, D)) ** 2, axis=-1)
+        assignments_new = np.argmin(dist, axis=1)
+        # update the centers
+        for j in range(k):
+            centers[j] = np.mean(features[assignments_new == j], axis=0)
+
+        # stop if converged
+        if np.all(assignments == assignments_new):
+            break
+        assignments = assignments_new.copy()
+        # END YOUR CODE
 
     return assignments
+
 
 def kmeans_fast(features, k, num_iters=100):
     """ Use kmeans algorithm to group features into k clusters.
@@ -80,12 +100,20 @@ def kmeans_fast(features, k, num_iters=100):
     assignments = np.zeros(N)
 
     for n in range(num_iters):
-        ### YOUR CODE HERE
-        pass
-        ### END YOUR CODE
+        # YOUR CODE HERE
+        dist = np.sum((features.reshape(N, 1, D) - centers.reshape(1, -1, D)) ** 2, axis=-1)
+        assignments_new = np.argmin(dist, axis=1)
+        # update the centers
+        for j in range(k):
+            centers[j] = np.mean(features[assignments_new == j], axis=0)
+
+        # stop if converged
+        if np.all(assignments == assignments_new):
+            break
+        assignments = assignments_new.copy()
+        # END YOUR CODE
 
     return assignments
-
 
 
 def hierarchical_clustering(features, k):
@@ -132,14 +160,31 @@ def hierarchical_clustering(features, k):
     n_clusters = N
 
     while n_clusters > k:
-        ### YOUR CODE HERE
-        pass
-        ### END YOUR CODE
+        # YOUR CODE HERE
+        dist = squareform(pdist(centers, 'euclidean'))
+        # we do not consider the distance of a point and itself
+        np.fill_diagonal(dist, np.inf)
+        # find the pair of clusters that have least distance (over all pairs)
+        pair = (np.argmin(dist) // dist.shape[0], np.argmin(dist) % dist.shape[1])
+        n_clusters -= 1
+
+        # merge the clusters and update the centers
+        centers[pair[0]] = (centers[pair[0]] + centers[pair[1]]) / 2
+        # delete the merged center
+        centers = np.delete(centers, pair[1], 0)
+
+        # update assignments, we only need to consider the points that are assigned to pair[1],
+        # and those points that are assigned to the clusters that have larger indices than pair[1],
+        # since these clusters indices will be shifted as we merge and the pair[1] to pair[0]
+        for i in range(N):
+            if assignments[i] >= pair[1]:
+                assignments[i] = pair[0] if assignments[i] == pair[1] else assignments[i] - 1
+        # END YOUR CODE
 
     return assignments
 
 
-### Pixel-Level Features
+# Pixel-Level Features
 def color_features(img):
     """ Represents a pixel by its color.
 
@@ -153,11 +198,12 @@ def color_features(img):
     img = img_as_float(img)
     features = np.zeros((H*W, C))
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    features = img.reshape(features.shape)
+    # END YOUR CODE
 
     return features
+
 
 def color_position_features(img):
     """ Represents a pixel by its color and position.
@@ -182,11 +228,16 @@ def color_position_features(img):
     color = img_as_float(img)
     features = np.zeros((H*W, C+2))
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    color_feat = color_features(img)
+    pos_feat = np.transpose(np.mgrid[0:H, 0:W], (1, 2, 0)).reshape(H*W, -1)
+    features = np.concatenate((color_feat, pos_feat), axis=1)
+    # normalize features
+    features = (features - np.mean(features, axis=0)) / np.std(features, axis=0, ddof=1)
+    # END YOUR CODE
 
     return features
+
 
 def my_features(img):
     """ Implement your own features
@@ -198,13 +249,13 @@ def my_features(img):
         features - array of (H * W, C)
     """
     features = None
-    ### YOUR CODE HERE
+    # YOUR CODE HERE
     pass
-    ### END YOUR CODE
+    # END YOUR CODE
     return features
 
 
-### Quantitative Evaluation
+# Quantitative Evaluation
 def compute_accuracy(mask_gt, mask):
     """ Compute the pixel-wise accuracy of a foreground-background segmentation
         given a ground truth segmentation.
@@ -222,11 +273,12 @@ def compute_accuracy(mask_gt, mask):
     """
 
     accuracy = None
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    accuracy = np.mean(mask_gt == mask)
+    # END YOUR CODE
 
     return accuracy
+
 
 def evaluate_segmentation(mask_gt, segments):
     """ Compare the estimated segmentation with the ground truth.
